72ce90d74a8b47d7dd2bd4c791177b60f3d82bda
César Nascimento <ceesar90@gmail.com>
2018-11-19 17:36:47 -0200

    Selection completo

--- src/selection/selection.java
+++ src/selection/selection.java
@@ -25,19 +25,45 @@ public class selection {//reaproveitando o método como uma função
     	
         int[] vet5 = new int[5];
         int[] vet10 = new int[10];
+        int[] vet50 = new int[50];
+        int[] vet100 = new int[100];
+        int[] vet1000 = new int[1000];
+        int[] vet10000 = new int[10000];
         
         //vet5 não ordenados
         System.out.println("Vetores não ordenados: ");
         for (int i = 0; i < vet5.length; i++) {
         	vet5[i] = r.nextInt(5);
-        	System.out.println(vet5[i]);
 		}
         
         //vet10 não ordenados
         System.out.println("Vetores não ordenados: ");
         for (int i = 0; i < vet10.length; i++) {
         	vet10[i] = r.nextInt(10);
-        	System.out.println(vet10[i]);
+		}
+        
+        //vet50 não ordenados
+        System.out.println("Vetores não ordenados: ");
+        for (int i = 0; i < vet50.length; i++) {
+        	vet50[i] = r.nextInt(50);
+		}
+        
+        //vet100 não ordenados
+        System.out.println("Vetores não ordenados: ");
+        for (int i = 0; i < vet100.length; i++) {
+        	vet100[i] = r.nextInt(100);
+		}
+        
+        //vet1000 não ordenados
+        System.out.println("Vetores não ordenados: ");
+        for (int i = 0; i < vet1000.length; i++) {
+        	vet1000[i] = r.nextInt(1000);
+		}
+        
+        //vet10000 não ordenados
+        System.out.println("Vetores não ordenados: ");
+        for (int i = 0; i < vet10000.length; i++) {
+        	vet10000[i] = r.nextInt(10000);
 		}
         
         //--------------------------------------------//
@@ -63,5 +89,49 @@ public class selection {//reaproveitando o método como uma função
             cont10++;
         }
         System.out.println("\nTotal de comparações com 10: "+cont10);
+        
+        //vet50 ordenados
+        int[] arr4 = doSelectionSort(vet50);
+        int cont50 = 0;
+        System.out.println("\nVetores com 50 ordenados: ");
+        for(int i:arr4){
+            System.out.print(i);
+            System.out.print(", ");
+            cont50++;
+        }
+        System.out.println("\nTotal de comparações com 50: "+cont50);
+        
+        //vet100 ordenados
+        int[] arr5 = doSelectionSort(vet100);
+        int cont100 = 0;
+        System.out.println("\nVetores com 100 ordenados: ");
+        for(int i:arr5){
+            System.out.print(i);
+            System.out.print(", ");
+            cont100++;
+        }
+        System.out.println("\nTotal de comparações com 100: "+cont100);
+        
+        //vet1000 ordenados
+        int[] arr6 = doSelectionSort(vet1000);
+        int cont1000 = 0;
+        System.out.println("\nVetores com 1000 ordenados: ");
+        for(int i:arr6){
+            System.out.print(i);
+            System.out.print(", ");
+            cont1000++;
+        }
+        System.out.println("\nTotal de comparações com 1000: "+cont1000);
+        
+        //vet10000 ordenados
+        int[] arr7 = doSelectionSort(vet10000);
+        int cont10000 = 0;
+        System.out.println("\nVetores com 10000 ordenados: ");
+        for(int i:arr7){
+            System.out.print(i);
+            System.out.print(", ");
+            cont10000++;
+        }
+        System.out.println("\nTotal de comparações com 10000: "+cont10000);
     }
 }

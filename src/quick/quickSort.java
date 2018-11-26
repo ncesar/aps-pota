5f5904ad8cc24bb15147ad34b00800a72773445d
César Nascimento <ceesar90@gmail.com>
2018-11-26 20:44:56 -0200

    Create quickSort.java

---
+++ src/quick/quickSort.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author cesarn
+ */
+public class JavaApplication1 {
+
+    public static void quickSort(int arr[], int begin, int end)
+    {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            // Recursively sort elements of the 2 sub-arrays
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+
+    private static int partition(int arr[], int begin, int end)
+    {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j=begin; j<end; j++)
+        {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }
+    
+}

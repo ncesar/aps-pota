f35db65aa205b4955ef7d6057df3f08bc6db64c1
César Nascimento <ceesar90@gmail.com>
2018-11-26 20:49:41 -0200

    Create bucketSort.java

---
+++ src/sort/bucketSort.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author cesarn
+ */
+public class JavaApplication1 {
+public static void sort(int[] a, int maxVal) {
+      int [] bucket=new int[maxVal+1];
+ 
+      for (int i=0; i<bucket.length; i++) {
+         bucket[i]=0;
+      }
+ 
+      for (int i=0; i<a.length; i++) {
+         bucket[a[i]]++;
+      }
+ 
+      int outPos=0;
+      for (int i=0; i<bucket.length; i++) {
+         for (int j=0; j<bucket[i]; j++) {
+            a[outPos++]=i;
+         }
+      }
+   }
+ 
+ 
+   public static void main(String[] args) {
+      int maxVal=5;
+      int [] data= {5,3,0,2,4,1,0,5,2,3,1,4}; 
+ 
+      System.out.println("Before: " + Arrays.toString(data));
+      sort(data,maxVal);
+      System.out.println("After:  " + Arrays.toString(data));
+   }
+    
+}
